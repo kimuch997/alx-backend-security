f8a172215a0855cd815d3f16bfd21cb5ff6bfc5a
kimuch <lxanthonykim@gmail.com>
2025-11-06 15:37:43 +0300

    Task2

--- alx_security_project/ip_tracking/middleware.py
+++ alx_security_project/ip_tracking/middleware.py
@@ -1,6 +1,8 @@
+from django.core.cache import cache
 from django.http import HttpResponseForbidden
 from .models import RequestLog,BlockedIP
 from django.utils.timezone import now
+import requests
 
 class IPLoggingMiddleware:
     def __init__(self,get_response):
@@ -12,6 +14,20 @@ class IPLoggingMiddleware:
 
         if BlockedIP.objects.filter(ip_address=ip_address).exists():
             return HttpResponseForbidden("Access denied: your IP has been blocked.")
+
+        cached_data = cache.get(ip_address)
+        if cached_data:
+            country,city = cached_data
+        else:
+            # Step 3:Fetch geolocation via external API()
+            try:
+                response = requests.get(f"https//ipinfo.io/{ip_address}/json")
+                data = response.json
+                country = data.get("city","")
+                # cache for 24 hrs
+                cache.set(ip_address,(country,city),timeout=60*60*24)
+            except Exception:
+                country,city="", ""
 
         # Log details
         RequestLog.objects.create(
